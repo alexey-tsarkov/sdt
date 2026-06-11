e07a111a6a329aa443771aca08df4a881a032ded
Alex Tsarkov <alex.tsarkov@gmail.com>
2026-06-11 11:15:26 +0300

    Refactor

--- db/02_a.sql
+++ db/02_a.sql
@@ -1,8 +1,8 @@
 -- Выбрать имена (name) всех клиентов, которые не делали заказы в последние 7 дней
 SELECT c.name
-FROM clients c
-LEFT JOIN orders o ON c.id = o.customer_id 
-    AND o.order_date >= DATE_SUB(NOW(), INTERVAL 7 DAY)
+FROM clients AS c
+LEFT JOIN orders AS o
+    ON c.id = o.customer_id AND o.order_date >= DATE_SUB(NOW(), INTERVAL 7 DAY)
 WHERE o.id IS NULL;
 
 DROP INDEX IF EXISTS idx_clients_name ON clients;
@@ -10,9 +10,9 @@ DROP INDEX IF EXISTS idx_orders_customer_date ON orders;
 
 -- Без индексов
 EXPLAIN SELECT c.name
-FROM clients c
-LEFT JOIN orders o ON c.id = o.customer_id 
-    AND o.order_date >= DATE_SUB(NOW(), INTERVAL 7 DAY)
+FROM clients AS c
+LEFT JOIN orders AS o
+    ON c.id = o.customer_id AND o.order_date >= DATE_SUB(NOW(), INTERVAL 7 DAY)
 WHERE o.id IS NULL;
 
 -- Можно добавить покрывающий индекс, но это приведет к повышенному расходу памяти из-за строкового поля и замеделнию записи
@@ -22,7 +22,7 @@ CREATE INDEX idx_orders_customer_date ON orders (customer_id, order_date);
 
 -- С индексами
 EXPLAIN SELECT c.name
-FROM clients c
-LEFT JOIN orders o ON c.id = o.customer_id 
-    AND o.order_date >= DATE_SUB(NOW(), INTERVAL 7 DAY)
+FROM clients AS c
+LEFT JOIN orders AS o
+    ON c.id = o.customer_id AND o.order_date >= DATE_SUB(NOW(), INTERVAL 7 DAY)
 WHERE o.id IS NULL;
